af68c028801e4fbdd38527de690353d2e29808cc
evansmj1 <michael.j.evans@vanderbilt.edu>
2017-08-30 15:59:51 -0500

    Added C# Scripts

---
+++ Scripts/HeightAdjust.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightAdjust : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.H))
+        {
+            float x = gameObject.transform.position.x;
+            float y = gameObject.transform.position.y - (float).01;
+            float z = gameObject.transform.position.z;
+            gameObject.transform.SetPositionAndRotation(new Vector3(x, y, z), new Quaternion());
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            float x = gameObject.transform.position.x;
+            float y = gameObject.transform.position.y + (float).01;
+            float z = gameObject.transform.position.z;
+            gameObject.transform.SetPositionAndRotation(new Vector3(x, y, z), new Quaternion());
+        }
+    }
+}
---
+++ Scripts/text3d.cs
@@ -0,0 +1,30 @@
+﻿ using UnityEngine;
+ 
+  using System.Collections;
+ 
+  public class text3d : MonoBehaviour 
+  {
+ 
+     TextMesh textObject;
+ 
+     // Use this for initialization
+     void Start () {
+     
+         textObject= gameObject.GetComponent("TextMesh") as TextMesh;
+     }
+     
+     // Update is called once per frame
+     void Update () {
+        int count = UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController.collectibleCount;
+        if (count < 7)
+        {
+            textObject.text = "Collectibles: " + count.ToString() + "/7";
+        }
+        else
+        {
+            textObject.text = "Congratulations!";
+        }
+     }
+  }
+ 
+ 
